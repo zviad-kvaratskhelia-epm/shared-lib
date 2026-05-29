52cbbbddd18753f9860524805d6b9aea93d2fc67
Zviad Kvaratskhelia <zviad_kvaratskhelia@epam.com>
2026-05-29 13:20:09 +0400

    Add docker login

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -3,9 +3,16 @@ def call (Map config) {
         def imageName = config.environment == "main" ? "nodemain:${config.imageTag}" : "nodedev:${config.imageTag}"
         def containerName = "node${config.environment}"
         def port = config.environment == "main" ? "3000" : "3001"
+        withCredentials([usernamePassword(
+                    credentialsId: 'c56850e3-4517-420b-b6e1-1f7d78eee9ba',
+                    usernameVariable: 'USER',
+                    passwordVariable: 'PASS'
+                )]) {
         sh "docker stop ${containerName} || true"
         sh "docker rm ${containerName} || true"
+        sh "docker login -u ${USER} -p ${PASS}"
         sh "docker pull ${imageName}"
         sh "docker run -d --name ${containerName} -p ${port}:3000 ${imageName}"
+        }
     }
 }
